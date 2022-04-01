83820b9e1a252856cd211ab972fb673637608aeb
Theo Galy-Fajou <theo.galyfajou@gmail.com>
2022-04-01 18:09:32 +0200

    Readapt examples generation

--- docs/make.jl
+++ docs/make.jl
@@ -1,40 +1,14 @@
 ## First process examples
-const EXAMPLES_OUT = joinpath(@__DIR__, "src", "examples")
-ispath(EXAMPLES_OUT) && rm(EXAMPLES_OUT; recursive=true)
-mkpath(EXAMPLES_OUT)
+using Pkg
+Pkg.add(Pkg.PackageSpec(; url="https://github.com/JuliaGaussianProcesses/JuliaGPsDocs.jl")) 
 
-# TODO use the general approach of ApproximateGPs.jl
-examples = filter!(isdir, readdir(joinpath(@__DIR__, "..", "examples"); join=true))
-let script = "using Pkg; Pkg.activate(ARGS[1]); Pkg.instantiate()"
-    for example in examples
-        if !success(`$(Base.julia_cmd()) -e $script $example`)
-            error(
-                "project environment of example ",
-                basename(example),
-                " could not be instantiated",
-            )
-        end
-    end
-end
-# Run examples asynchronously
-processes = let literatejl = joinpath(@__DIR__, "literate.jl")
-    map(examples) do example
-        return run(
-            pipeline(
-                `$(Base.julia_cmd()) $literatejl $(basename(example)) $EXAMPLES_OUT`;
-                stdin=devnull,
-                stdout=devnull,
-                stderr=stderr,
-            );
-            wait=false,
-        )::Base.Process
-    end
-end
-# Check that all examples were run successfully
-isempty(processes) || success(processes) || error("some examples were not run successfully")
 
 ## Build the docs
 using AugmentedGPLikelihoods
+using JuliaGPsDocs
+
+JuliaGPsDocs.generate_examples(AugmentedGPLikelihoods)
+
 using Documenter
 using DocumenterCitations
 using Literate
@@ -63,8 +37,10 @@ makedocs(
     ),
     pages=[
         "Home" => "index.md",
-        "Likelihoods" => joinpath.(Ref("likelihoods"), likelihoods),
-        "Examples" => joinpath.(Ref("examples"), likelihoods),
+        "Likelihoods" => readdir("likelihoods"; join=true),
+        "Examples" => map(filter!(isdir, readdir("examples"))) do x
+            joinpath("examples", x, "example.md")
+        end,
         "Misc" => "misc.md",
         "References" => "references.md",
     ],
