329a38e7de11f8e5ef2b70ed6f5819d1a22a1ee5
Adrià Garriga-Alonso <adria.garriga@gmail.com>
2023-02-02 16:12:30 -0800

    Add reference logpdf test

--- test/SpecialDistributions/polyagamma.jl
+++ test/SpecialDistributions/polyagamma.jl
@@ -1,10 +1,37 @@
+using IrrationalConstants: logtwo, log2π
+
+function ref_logpdf(d::PolyaGamma, x::Real)
+    b, c = Distributions.params(d)
+    if iszero(b)
+        return iszero(x) ? zero(x) : -Inf # The limit of PG when b->0
+    # is the delta dirac at 0.
+    else
+        iszero(x) && -Inf # The limit to p(x) for x-> 0 is 0.
+        ext = ref_logtilt(x, b, c) + (b - 1) * logtwo - loggamma(b) - (log2π + 3 * log(x)) / 2
+        pos_val = logsumexp(ref_pdf_val_log_series(n, b, x) for n in 0:2:201)
+        neg_val = logsumexp(ref_pdf_val_log_series(n, b, x) for n in 1:2:201)
+        return ext + log(exp(pos_val) - exp(neg_val))
+    end
+end
+
+function ref_pdf_val_log_series(n::Integer, b::Real, x)
+    return loggamma(n + b) - loggamma(n + 1) - abs2(2n + b) / (8x) + log(2n + b) # all terms where n is present
+end
+
+function ref_logtilt(ω, b, c)
+    return b * logcosh(c / 2) - abs2(c) * ω / 2
+end
+
+
 @testset "polyagamma" begin
     @test mean(PolyaGamma(1, 0)) == 1 / 4
     @test mean(PolyaGamma(1, 2.0)) == tanh(1.0) / 4
 
     for (b, c) in ((1, 0), (1, 2.0), (3, 0), (3, 2.5), (3, 3.2), (1.2, 3.2))
         p = PolyaGamma(b, c)
-        @test all(isreal, logpdf.(p, 10.0 * rand(p, 1000)))
+        xs = rand(p, 1000)
+        # @test all(isreal, logpdf.(p, 100*xs))
+        @test ref_logpdf.(p, xs) ≈ logpdf.(p, xs)
         @test mean(rand(p, 10000)) ≈ mean(p) atol = 1e-2
     end
 
